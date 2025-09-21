4594a2d597610a6f1aa4e073a3a4d2a846e26b62
Dev_botz <deveshpareek001@gmail.com>
2025-09-21 13:09:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ DB_CHANGE_LIMIT = int(environ.get('DB_CHANGE_LIMIT', "432"))
 
 # Channel links
 
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_new_movies_2025')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movies_2025_xx')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+7e7jpfbA2580MTZl')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Ruler_boy')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+7e7jpfbA2580MTZl')
