a6241cf76ec8aaddc7b65613c7baed1f1804a49d
Dev_botz <deveshpareek001@gmail.com>
2026-03-11 16:43:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+xTKKXSX80o
 
 # Auth Channels
 
-AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1003033922040") # add multiple channels here, separated by single space
+AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "") # add multiple channels here, separated by single space
 AUTH_CHANNEL = [int(ch) for ch in AUTH_CHANNEL.strip().split()] if AUTH_CHANNEL else []
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '') # add multiple channels here, separated by single space
 AUTH_REQ_CHANNEL = [int(ch) for ch in AUTH_REQ_CHANNEL.strip().split()] if AUTH_REQ_CHANNEL else []
